a701fd04d8271753b95039bc65fb867c34e16b8b
Christopher <christopher.ronnberg027@gmail.com>
2020-05-07 18:48:42 +0200

    Layout fixes

--- src/views/Bookshelf.vue
+++ src/views/Bookshelf.vue
@@ -1,18 +1,18 @@
 <template>
-<div class="wrapper">
-  <h1>Classic Children books</h1>
-  <section class="bookshelf">
-    <div class="books-container">
-      <BookThumb
-      v-for="book in bookList"
-      v-bind:key="book.id"
-      v-bind:id="book.id"
-      v-bind:title="book.title"
-      v-bind:author="book.author"
-      v-bind:color="book.color"
-      />
-    </div>
-  </section>
+  <div class="wrapper">
+    <h1>Classic Children books</h1>
+    <section class="bookshelf">
+      <div class="books-container">
+        <BookThumb
+          v-for="book in bookList"
+          v-bind:key="book.id"
+          v-bind:id="book.id"
+          v-bind:title="book.title"
+          v-bind:author="book.author"
+          v-bind:color="book.color"
+        />
+      </div>
+    </section>
   </div>
 </template>
 
@@ -21,7 +21,7 @@ import BookThumb from "../components/BookThumb";
 export default {
   data() {
     return {
-      bookList : this.$root.booksDB
+      bookList: this.$root.booksDB
     };
   },
   name: "BookShelf",
@@ -31,10 +31,19 @@ export default {
 };
 </script>
 <style scoped>
-.books-container{
+.wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+h1{
+  font-size: 6em;
+  font-weight: 800;
+}
+.books-container {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
 }
-
-
 </style>
